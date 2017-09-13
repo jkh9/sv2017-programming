a4ed0953aeab19e4e5b7612899e75c206c2b4dde
Nacho Cabanes <nacho@nachocabanes.com>
2017-09-13 23:56:20 +0200

    Exercise 3: Several texts in several lines

---
+++ chapter01-contactWithCSharp/003-Three.cs
@@ -0,0 +1,9 @@
+public class Three
+{
+	public static void Main()
+	{
+		System.Console.WriteLine("First line");
+		System.Console.WriteLine("Second line");
+		System.Console.WriteLine("Third line");
+	}
+}
