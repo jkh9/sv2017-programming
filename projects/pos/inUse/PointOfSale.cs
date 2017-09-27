30c2d19d7287ae17bada888bbcfc481ce254fdb5
Nacho Cabanes <ncabanes@users.noreply.github.com>
2017-09-27 17:56:22 +0200

    PointOfSale V2: Analyse user input

--- projects/pos/inUse/PointOfSale.cs
+++ projects/pos/inUse/PointOfSale.cs
@@ -1,8 +1,10 @@
-// LUCÍA NAVARRO VÉLEZ
+// 
 // Point of sale
+//
 
 // Versions:
-// V0.01 20-Sep-2017 Main menu
+// V0.01 20-Sep-2017 LUCÍA NAVARRO VÉLEZ, Main menu
+// V0.02 27-Sep-2017 Javier Herreros, Analyse user input
 
 using System;
 public class PointOfSale
@@ -12,9 +14,21 @@ public class PointOfSale
         int option;
         Console.WriteLine("1-Sell");
         Console.WriteLine("2-Management");
-        Console.WriteLine("3-Exit");
-        Console.WriteLine("");
+        Console.WriteLine("0-Exit");
+        Console.WriteLine();
         Console.WriteLine("Choose one option");
         option = Convert.ToInt32(Console.ReadLine());
+        
+        if (option == 1)
+            Console.WriteLine("Soon");
+        
+        else if (option == 2)
+            Console.WriteLine("Soon");
+        
+        else if (option == 0)
+            Console.WriteLine("Bye!");
+            
+        else 
+            Console.WriteLine("Wrong option");
     }
 }
