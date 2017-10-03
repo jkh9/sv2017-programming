a8275cc2f37d4d1b6239e1e05e565bf8386bd291
Nacho Cabanes <nacho@nachocabanes.com>
2017-10-03 09:48:12 +0200

    "if" vs "switch": names of the months

---
+++ chapter02-controlStructures/067-NameOfMonthsSwitch.cs
@@ -0,0 +1,33 @@
+//Daniel Miquel Sirera
+//months with Switch
+
+using System;
+
+public class MonthsSwitch
+{    
+    public static void Main()
+    {
+        int number;
+        Console.Write("Number of the month? ");
+        number = Convert.ToInt32(Console.ReadLine());
+        
+        switch(number)
+        {
+            case 1: Console.WriteLine("January"); break;
+            case 2: Console.WriteLine("February"); break;
+            case 3: Console.WriteLine("March"); break;
+            case 4: Console.WriteLine("April"); break;
+            case 5: Console.WriteLine("May"); break;
+            case 6: Console.WriteLine("June"); break;
+            case 7: Console.WriteLine("July"); break;
+            case 8: Console.WriteLine("August"); break;
+            case 9: Console.WriteLine("September"); break;
+            case 10: Console.WriteLine("October"); break;
+            case 11: Console.WriteLine("November"); break;
+            case 12: Console.WriteLine("December"); break;
+            default: Console.WriteLine("Unknown month"); break;
+        }
+    }
+}
+            
+            
