191c9f847c519be41f84b0c0101bf222a5d66e4c
Nacho Cabanes <nacho@nachocabanes.com>
2017-10-05 12:57:47 +0200

    Ex.071: Marks, version using "switch"

---
+++ chapter02-controlStructures/071c-MarksSwitch3.cs
@@ -0,0 +1,53 @@
+/*
+Create a C# program to display the "text mark" corresponding to a certain 
+"numerical mark", using the following equivalence:
+
+9,10 = Sobresaliente
+7,8 = Notable
+6 = Bien
+5 = Aprobado
+0-4 = Suspenso
+
+Your program must ask the user for a numerical mark and display the 
+corresponding text mark.
+*/
+
+using System;
+public class Marks3
+{
+    public static void Main()
+    {
+        int mark;
+        
+        Console.WriteLine("Enter the mark");
+        mark = Convert.ToInt32(Console.ReadLine());
+        
+        switch (mark)
+        {
+            case 0:
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+                Console.WriteLine("Suspenso");
+                break;
+            case 5:
+                Console.WriteLine("Suficiente");
+                break;
+            case 6:
+                Console.WriteLine("Bien");
+                break;
+            case 7:
+            case 8:
+                Console.WriteLine("Notable");
+                break;
+            case 9:
+            case 10:
+                Console.WriteLine("Sobresaliente");
+                break;
+            default:
+                Console.WriteLine("???");
+                break;
+        }
+    }
+}
