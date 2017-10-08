725cb3a28fbbb925ca0b65deb5f008a3ce39bba0
Nacho Cabanes <nacho@nachocabanes.com>
2017-10-08 23:37:27 +0200

    Exercise 83: char + for

---
+++ chapter03-dataTypes/083-CharAToZ.cs
@@ -0,0 +1,15 @@
+/*
+    A to Z
+    Francisco Sabater Villora, 2017
+*/
+using System;
+public class AtoZ
+{
+    public static void Main()
+    {
+        for( char letter = 'A'; letter <= 'Z'; letter++)
+        {
+            Console.Write("{0} ", letter);
+        }
+    }
+}
