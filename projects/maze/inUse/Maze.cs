2a44957329c715d705eb91510e9469fb44f847fa
Nacho Cabanes <ncabanes@users.noreply.github.com>
2017-10-11 16:16:11 +0200

    Maze v2: Main menu + loop

--- projects/maze/inUse/Maze.cs
+++ projects/maze/inUse/Maze.cs
@@ -1,9 +1,8 @@
 /*
  *  Maze Game
  * 
- *  Francisco Sabater Villora, 2017
- * 
- *  V0.01 20-Sep-2017 Main menu
+ *  V0.01 20-Sep-2017 Francisco Sabater: Main menu
+ *  V0.02 29-Sep-2017 Gonzalo Martinez: Main menu do-while
  */
  
 using System;
@@ -14,15 +13,59 @@ public class Maze
     {
         int option;
         
-        Console.WriteLine("1.- New Game.");
-        Console.WriteLine("2.- Load Game.");
-        Console.WriteLine("3.- Help.");
-        Console.WriteLine("4.- Credits.");
-        Console.WriteLine("5.- Options.");
-        Console.WriteLine("0.- Exit.");
-        Console.Write("Select an option: ");
-        option = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine();
-        Console.WriteLine("Option not available... yet");
+        do
+        {
+            Console.WriteLine("1.- New Game.");
+            Console.WriteLine("2.- Load Game.");
+            Console.WriteLine("3.- Help.");
+            Console.WriteLine("4.- Credits.");
+            Console.WriteLine("5.- Options.");
+            Console.WriteLine("0.- Exit.");
+            
+            Console.Write("Select an option: ");
+            option = Convert.ToInt32(Console.ReadLine());
+            
+            if(option == 1)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Option not available... yet");
+                Console.WriteLine();
+            }
+            else if(option == 2)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Option not available... yet");
+                Console.WriteLine();
+            }
+            else if(option == 3)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Option not available... yet");
+                Console.WriteLine();
+            }
+            else if(option == 4)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Option not available... yet");
+                Console.WriteLine();
+            }
+            else if(option == 5)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Option not available... yet"); 
+                Console.WriteLine();  
+            }
+            else if(option == 0)
+            {
+                Console.WriteLine("Bye!");
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine("Wrong option");
+                Console.WriteLine(); 
+            }
+        }
+        while(option != 0);
     }
 }
