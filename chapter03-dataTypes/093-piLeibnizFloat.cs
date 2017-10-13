e57ae5e19d7b9c1ccc0897fe1e21140d1aaeaecf
Nacho Cabanes <nacho@nachocabanes.com>
2017-10-13 22:58:36 +0200

    Exercise 92 & 03: Pi + Leibniz formula + double & float

---
+++ chapter03-dataTypes/093-piLeibnizFloat.cs
@@ -0,0 +1,33 @@
+/*
+Create a program to calculate an approximation for PI using the 
+expression
+
+pi/4 = 1/1 - 1/3 + 1/5 -1/7 + 1/9 - 1/11 + 1/13 ...
+
+The user will indicate how many terms must be used, and the program 
+will display all the results until that amount of terms.
+*/
+
+using System;
+
+public class PiLeibniz
+{
+    public static void Main()
+    {
+        int terms;
+        float numerator = 1.0f;
+        float denominator = 1;
+        float piFourths = 0;
+        
+        Console.Write("How many terms to estimate PI? ");
+        terms = Convert.ToInt32( Console.ReadLine() );
+        
+        for (int i = 0; i < terms; i++ )
+        {
+            piFourths += numerator / denominator;
+            numerator = -numerator;
+            denominator += 2;
+        }
+        Console.WriteLine( piFourths * 4 );
+    }
+}
