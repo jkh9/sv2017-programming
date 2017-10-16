8e22a75be4a1906c16068b8cd513f5a0d90d62bf
Nacho Cabanes <ncabanes@users.noreply.github.com>
2017-10-16 20:38:29 +0200

    Area and volume of a sphere

---
+++ chapter03-dataTypes/103-SphereDouble.cs
@@ -0,0 +1,20 @@
+﻿// Area and volume of a sphere (double)
+
+using System;
+
+class Sphere
+{
+    static void Main()
+    {
+        double pi = 3.1415926535;
+        
+        Console.Write("Enter radius: ");
+        double radius = Convert.ToDouble(Console.ReadLine());
+
+        double area = 4 * pi * radius * radius;
+        Console.WriteLine("Area = " + area);
+
+        double volume = 4.0 / 3.0 * pi * radius *  radius * radius;
+        Console.WriteLine("Volume = " + volume);
+    }
+}
---
+++ chapter03-dataTypes/104-SphereFloat.cs
@@ -0,0 +1,20 @@
+﻿// Area and volume of a sphere (float)
+
+using System;
+
+class Sphere
+{
+    static void Main()
+    {
+        float pi = 3.1415926535f;
+        
+        Console.Write("Enter radius: ");
+        float radius = Convert.ToSingle(Console.ReadLine());
+
+        float area = 4 * pi * radius * radius;
+        Console.WriteLine("Area = " + area);
+
+        float volume = 4.0f / 3.0f * pi * radius *  radius * radius;
+        Console.WriteLine("Volume = " + volume);
+    }
+}
