5603c8c5c04f13c9593d67cb8eb86c00f125d27d
Nacho Cabanes <ncabanes@users.noreply.github.com>
2017-10-24 19:54:07 +0200

    Additional example of "var"

---
+++ chapter03-dataTypes/130b-VarExample.cs
@@ -0,0 +1,18 @@
+// Example of "var"
+
+using System;
+
+public class VarExample
+{
+    public static void Main()
+    {
+        var n = 5;
+        var condition = 5 == 7;
+        var letter = 'a';
+        var pi = 3.1416;
+        var text = "Hello";
+        
+        Console.WriteLine(pi);
+        Console.WriteLine(pi.GetType());
+    }
+}
