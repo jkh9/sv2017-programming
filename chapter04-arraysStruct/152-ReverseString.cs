47d08e380261d787c54e9b3f0e9b70c1de141e84
Nacho Cabanes <nacho@nachocabanes.com>
2017-10-30 21:44:48 +0100

    Ex.152: Reverse a string

---
+++ chapter04-arraysStruct/152-ReverseString.cs
@@ -0,0 +1,18 @@
+﻿// Exercise 152: Reverse a string
+// Javier Cases Sempere
+
+using System;
+
+public class Ex152
+{
+    public static void Main()
+    {
+        Console.Write("Enter some text: ");
+        string text = Console.ReadLine();
+
+        for (int i = text.Length - 1; i >= 0; i--)
+            Console.Write(text[i]);
+
+        Console.WriteLine();
+    }
+}
