d4134c6627620772a8ac18947046082fc040ef4c
Nacho Cabanes <ncabanes@users.noreply.github.com>
2017-11-02 19:30:17 +0100

    Exercise 156: ToUpper (2 versions)

---
+++ chapter04-arraysStruct/156a-ToUpper1.cs
@@ -0,0 +1,15 @@
+﻿// Exercise 156: ToUpper (version 1)
+
+using System;
+
+class Ex156a
+{
+    static void Main()
+    {
+        Console.WriteLine("Name? ");
+        string name = Console.ReadLine();
+
+        name = name.ToUpper();
+        Console.WriteLine(name);
+    }
+}
---
+++ chapter04-arraysStruct/156b-ToUpper2.cs
@@ -0,0 +1,14 @@
+﻿// Exercise 156: ToUpper (version 2)
+
+using System;
+
+class Ex156
+{
+    static void Main()
+    {
+        Console.WriteLine("Name? ");
+        string name = Console.ReadLine();
+
+        Console.WriteLine( name.ToUpper() );
+    }
+}
