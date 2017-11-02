544f172de8f171c89ddff8aeb72b41b171314c8d
Nacho Cabanes <ncabanes@users.noreply.github.com>
2017-11-02 19:27:18 +0100

    Exercise 155: Triangle of a name

---
+++ chapter04-arraysStruct/155-TriangleOfName.cs
@@ -0,0 +1,18 @@
+﻿// Exercise 155: Triangle of a name
+
+using System;
+
+class Ex155
+{
+    static void Main()
+    {
+        Console.WriteLine("Name? ");
+        string name = Console.ReadLine();
+
+        for (int i = 1; i <= name.Length; i++)
+        {
+            Console.WriteLine(name.Substring(0,i));
+        }
+
+    }
+}
