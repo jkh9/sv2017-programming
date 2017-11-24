5b488ff292465d722d7cd7b857b045c627e60746
Nacho Cabanes <nacho@nachocabanes.com>
2017-11-24 22:31:32 +0100

    Ex.202: Function MaxArray

---
+++ chapter05-functions/202-MaxArray.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Ex202
+{
+    public static int Max(int[] x)
+    {
+        int max = x[0];
+        foreach (int n in x)
+            max = n > max ? n : max;
+        return max;
+    }
+    
+    public static void Main()
+    {
+        int[] x = { 10, 15, -5 };
+        Console.WriteLine(Max(x));
+    }
+}
