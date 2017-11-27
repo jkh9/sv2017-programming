fde26549ff3e8eeeb44dc0a07965593a814acfb9
Nacho Cabanes <nacho@nachocabanes.com>
2017-11-27 22:20:22 +0100

    Ex210 b: Twice + ref parameters

---
+++ chapter05-functions/210b-TwiceRef.cs
@@ -0,0 +1,19 @@
+﻿using System;
+public class NumberDoubled2b
+{
+    public static void Twice(ref int n)
+    {
+        n = n * 2;
+        Console.WriteLine("In the function: "+n);
+    }
+
+    public static void Main()
+    {
+        Console.Write("Number? ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        Twice(ref n);
+        Console.WriteLine("In Main: " + n);
+    }
+}
+
