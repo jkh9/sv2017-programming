c6af8c7ab7a194e1cd6bda426b3da263f1fdd0d7
Nacho Cabanes <nacho@nachocabanes.com>
2017-11-27 22:13:36 +0100

    Ex.207: Function MaxMinArray (ref parameters)

---
+++ chapter05-functions/207-MaxMinArray.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Ex207
+{
+    public static void MaxMinArray(int[] x, 
+        ref int max, ref int min)
+    {
+        max = x[0];
+        min = x[0];
+        foreach (int n in x)
+        {
+            max = n > max ? n : max;
+            min = n < min ? n : min;
+        }
+    }
+    
+    public static void Main()
+    {
+        int[] data = { 10, 15, -5 };
+        int maximum = 0, minimum = 0;
+        MaxMinArray(data, ref maximum, ref minimum);
+        Console.WriteLine("max = "+ maximum);
+        Console.WriteLine("min = "+ minimum);
+    }
+}
