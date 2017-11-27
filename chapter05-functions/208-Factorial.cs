93e03f2953a8cb594ba529f6a50dc9a32b3201e1
Nacho Cabanes <nacho@nachocabanes.com>
2017-11-27 22:14:38 +0100

    Ex.208: Factorial (iterative)

---
+++ chapter05-functions/208-Factorial.cs
@@ -0,0 +1,22 @@
+﻿//Javier herreros pozo
+
+using System;
+public class Factorial
+{
+    public static int Fact(int n)
+    {
+        int result = 1;
+        for(int i=n; i>=1; i--)
+            result *= i;
+        return result;
+    }
+
+    public static void Main()
+    {
+        Console.Write("Number? ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine(Fact(n));
+    }
+}
+
