e162206ab1fa4f72f25926922e74f48afe0c31ec
Nacho Cabanes <nacho@nachocabanes.com>
2017-11-27 22:19:26 +0100

    Ex210 a: Twice + modifying a parameter (first attempt)

---
+++ chapter05-functions/210a-TwiceRef-pre.cs
@@ -0,0 +1,19 @@
+﻿using System;
+public class NumberDoubled2
+{
+    public static void Twice(int n)
+    {
+        n = n * 2;
+        Console.WriteLine("In the function: "+n);
+    }
+
+    public static void Main()
+    {
+        Console.Write("Number? ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        Twice(n);
+        Console.WriteLine("In Main: " + n);
+    }
+}
+
