060051e9c021244f7dc2127094e1066ab1ac5949
Nacho Cabanes <nacho@nachocabanes.com>
2017-11-27 22:34:59 +0100

    Ex.211: WriteCenteredTriangle

---
+++ chapter05-functions/211-CenteredTriangle.cs
@@ -0,0 +1,32 @@
+﻿// Javier Cases Sempere
+
+using System;
+
+public class Ex211
+{
+    public static void WriteCentered(string text)
+    {
+        const byte width = 80;
+        for (int i = 0; i < (width / 2) - (text.Length / 2); i++)
+            Console.Write(" ");
+        Console.WriteLine(text);
+    }
+        
+    public static void WriteCenteredTriangle(int size, char symbol)
+    {
+        while (size > 0)
+        {
+            WriteCentered( new string(symbol, size) );
+            size -= 2;
+        }
+    }
+
+    public static void Main()
+    {
+        Console.Write("Size? ");
+        int size = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Char? ");
+        char symbol = Convert.ToChar(Console.ReadLine());
+        WriteCenteredTriangle(size, symbol);
+    }
+}
