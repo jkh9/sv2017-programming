d4b9485ed25829667fb7f48e3b8d4995975d4e77
Nacho Cabanes <nacho@nachocabanes.com>
2017-11-27 22:18:37 +0100

    Ex.209: Function "Twice"

---
+++ chapter05-functions/209-Twice.cs
@@ -0,0 +1,17 @@
+﻿using System;
+public class NumberDoubled
+{
+    public static int Twice(int n)
+    {
+        return n * 2;
+    }
+
+    public static void Main()
+    {
+        Console.Write("Number? ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine(Twice(n));
+    }
+}
+
