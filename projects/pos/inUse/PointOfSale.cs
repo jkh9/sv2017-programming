d02d25304cce31f8a404df3133146164693031d1
Nacho Cabanes <ncabanes@users.noreply.github.com>
2017-11-29 16:35:38 +0100

    Update PointOfSale.cs

--- projects/pos/inUse/PointOfSale.cs
+++ projects/pos/inUse/PointOfSale.cs
@@ -8,78 +8,23 @@
 // V0.03 27-Sep-2017 Brandon Blasco, Main loop
 // V0.04 11-Oct-2017 Guillermo Pastor, Switch
 // V0.05 10-Nov-2017 Moises+Miguel+Gonzalo: Sell option
-
+// V0.06 29-Nov-2017 Miguel Puerta: Functions
 
 using System;
 public class PointOfSale
 {
     public static void Main()
     {
-        double[] dailyTransactions = new double[1000];
-        int amountOfTrasactions = 0;
         int option;
 
         do
         {
-            Console.WriteLine("1 - Sell");
-            Console.WriteLine("2 - Management");
-            Console.WriteLine("0 - Exit");
-            Console.WriteLine();
-
-            Console.WriteLine("Choose one option: ");
+            ShowMenu();            
             option = Convert.ToInt32(Console.ReadLine());
-
             switch (option)
             {
-                case 1:
-                    Console.WriteLine("Hint: Enter the amount sold, "
-                        + "press Enter to get the total, or "
-                        + "type \"total\" or \"end\"");
-                    string answer;
-
-                    do  // Loop for a whole daily session
-                    { 
-                        double amount = 0;
-                        double sum = 0;
-
-                        do  // Loop for a single sell
-                        {
-                            Console.Write("Amount? ");
-                            answer = Console.ReadLine();
-
-                            try
-                            {
-                                amount = Convert.ToDouble(answer);
-                                sum += amount;
-                                dailyTransactions[amountOfTrasactions] = amount;
-                                amountOfTrasactions++;
-                            }
-                            catch (Exception)
-                            {
-                                // Nothing to do if it is not a valid number
-                            }
-                        }
-                        while ((answer != "") && (answer != "total") 
-                            && (answer != "end"));                   
-                        Console.WriteLine("Total: " + sum);
-                        sum = 0;
-
-                        if ((answer == "total") || (answer == "total"))
-                        {
-                            double total = 0;
-                            for (int i = 0; i < amountOfTrasactions; i++)
-                            {
-                                total += dailyTransactions[i];
-                            }
-                            Console.WriteLine("Daily total: "+total);
-                        }
-                    }
-                    while (answer != "end") ;
-                    break;
-                case 2:
-                    Console.WriteLine("Soon");
-                    Console.WriteLine();
-                    break;
+                case 1: Sell(); break;
+                case 2: Management(); break;
                 case 0:
                     Console.WriteLine("Bye!");
                     Console.WriteLine();
@@ -91,5 +36,70 @@ public class PointOfSale
             }
         }
         while (option != 0);
+    }
+
+    public static void ShowMenu()
+    {
+        Console.WriteLine("1 - Sell");
+        Console.WriteLine("2 - Management");
+        Console.WriteLine("0 - Exit");
+        Console.WriteLine();
+        Console.WriteLine("Choose one option: ");
+    }
+
+    public static void Sell()
+    {
+        double[] dailyTransactions = new double[1000];
+        int amountOfTransactions = 0;
+
+        Console.WriteLine("Hint: Enter the amount sold, "
+            + "press Enter to get the total, or "
+            + "type \"total\" or \"end\"");
+        string answer;
+
+        do  // Loop for a whole daily session
+        {
+            double amount = 0;
+            double sum = 0;
+
+            do  // Loop for a single sell
+            {
+                Console.Write("Amount? ");
+                answer = Console.ReadLine();
+
+                try
+                {
+                    amount = Convert.ToDouble(answer);
+                    sum += amount;
+                    dailyTransactions[amountOfTransactions] = amount;
+                    amountOfTransactions++;
+                }
+                catch (Exception)
+                {
+                    // Nothing to do if it is not a valid number
+                }
+            }
+            while ((answer != "") && (answer != "total")
+                && (answer != "end"));
+            Console.WriteLine("Total: " + sum);
+            sum = 0;
+
+            if ((answer == "total") || (answer == "total"))
+            {
+                double total = 0;
+                for (int i = 0; i < amountOfTransactions; i++)
+                {
+                    total += dailyTransactions[i];
+                }
+                Console.WriteLine("Daily total: " + total);
+            }
+        }
+        while (answer != "end");
+    }
+
+    public static void Management()
+    {
+        Console.WriteLine("Soon");
+        Console.WriteLine();
     }
 }
