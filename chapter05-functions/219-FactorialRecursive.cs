cefdc090dd7eba62808ee618b4192413c4905dcd
Nacho Cabanes <nacho@nachocabanes.com>
2017-11-30 22:36:40 +0100

    Ex.219: Factorial, Recursive

---
+++ chapter05-functions/219-FactorialRecursive.cs
@@ -0,0 +1,20 @@
+﻿// Factorial, recursion
+// Javier herreros pozo
+
+using System;
+public class FactorialRecursion
+{
+    public static long Factorial(long n)
+    {
+        if (n == 0)
+            return 1;
+        return n * Factorial(n - 1);
+    }
+    
+    public static void Main()
+    {
+        Console.Write("Num? ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Factorial = " + Factorial(n));
+    }
+}
