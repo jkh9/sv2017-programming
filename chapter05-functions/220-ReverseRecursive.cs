f2a119f96ff4191457bba7719d6a6381eb79c441
Nacho Cabanes <nacho@nachocabanes.com>
2017-12-01 11:45:27 +0100

    Ex.220: Function Reverse, recursive

---
+++ chapter05-functions/220-ReverseRecursive.cs
@@ -0,0 +1,24 @@
+﻿// Function Reverse, recursive version
+// Javier Cases Sempere
+
+using System;
+
+public class Ex220
+{
+    public static string Reverse(string text)
+    {
+        // Base case: 1 letter or empty string -> as is
+        if (text.Length <= 1)
+            return text;
+        // General case: last letter + previous n-1 reversed
+        return text.Substring(text.Length - 1, 1) + 
+            Reverse(text.Substring(0, text.Length - 1));
+    }
+
+    public static void Main()
+    {
+        string text = "Hola";
+        Console.WriteLine(Reverse(text));
+    }
+}
+
