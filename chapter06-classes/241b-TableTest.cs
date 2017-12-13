2a498ace07964b40a2823865601b9833d543cd59
Nacho Cabanes <ncabanes@users.noreply.github.com>
2017-12-13 17:03:57 +0100

    Ex.241: Classes Table and Leg

---
+++ chapter06-classes/241b-TableTest.cs
@@ -0,0 +1,40 @@
+// TableTest + Table + Leg
+
+// -------------------------------------------------------------
+
+
+class TableTest
+{
+    static void Main(string[] args)
+    {
+        Table t = new Table();
+        t.Open();
+    }
+}
+
+// -------------------------------------------------------------
+
+class Table
+{
+    Leg myLeg;
+    string color;
+
+    public void Open()
+    {
+        // TO DO
+    }
+
+    public void Close()
+    {
+        // TO DO
+    }
+}
+
+
+
+// -------------------------------------------------------------
+
+class Leg
+{
+    string color;
+}
