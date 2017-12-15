7994ac78eab2276af8214627e2a708e650f5e30f
Nacho Cabanes <ncabanes@users.noreply.github.com>
2017-12-15 19:01:28 +0100

    Ex.252: Class Sprite

---
+++ chapter06-classes/252-Sprite.cs
@@ -0,0 +1,34 @@
+﻿public class Sprite
+{
+    protected int x;
+    protected int y;
+    protected char symbol;
+
+    public Sprite(int newX, int newY, char newSymbol)
+    {
+        x = newX;
+        y = newY;
+        symbol = newSymbol;
+    }
+    
+    public Sprite()
+    {
+        x = 1;
+        y = 1;
+        symbol = 'X';
+    }
+
+    public int GetX() { return x; }
+    public int GetY() { return y; }
+    public char GetSymbol() { return symbol; }
+
+    public void SetX(int nx) { x = nx; }
+    public void SetY(int ny) { y = ny; }
+    public void SetSymbol(char s) { symbol = s; }
+
+    public void Display()
+    {
+        System.Console.SetCursorPosition(x, y);
+        System.Console.Write(symbol);
+    }
+}
