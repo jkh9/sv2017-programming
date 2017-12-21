d72ef69ae6a3186a13952fcb88a886b660371a45
Nacho Cabanes <ncabanes@users.noreply.github.com>
2017-12-21 17:00:04 +0100

    Ex.263: Class SocialNetwork+Properties

---
+++ chapter06-classes/263-SocialNetworkProperties.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class SocialNetwork
+{
+    protected string name;
+    protected string url;
+    protected int foundationYear;
+
+    public SocialNetwork(string newName, 
+        string newUrl, int newFoundationYear)
+    {
+        name = newName;
+        url = newUrl;
+        foundationYear = newFoundationYear;
+    }
+
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+    
+    public string Url
+    {
+        get { return url; }
+        set { url = value; }
+    }
+    
+    public int FoundationYear
+    {
+        get { return foundationYear; }
+        set { foundationYear = value; }
+    }
+}
+
+// --------------------------------
+
+class MyTest
+{
+    public static void Main()
+    {
+        SocialNetwork fb = new SocialNetwork(
+            "Facebook", "facebook.com", 2004);
+        Console.WriteLine(fb.Name + " was founded on "
+            + fb.FoundationYear );
+    }
+}
