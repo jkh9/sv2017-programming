6d95b1089d6c940c6aaf8edd834627013502d845
Nacho Cabanes <nacho@nachocabanes.com>
2017-12-21 13:29:02 +0100

    Ex.258: Overloaded constructor

---
+++ chapter06-classes/258-OverloadedConstructor.cs
@@ -0,0 +1,44 @@
+// Class Car, destructor + 2 constructors
+
+using System;
+
+class Car
+{
+    protected int wheels;
+   
+    public Car()
+    {
+        Console.WriteLine("Creating a car");
+        wheels = 4;
+    }   
+    
+    public Car(int newWheels)
+    {
+        Console.WriteLine("Creating a car with {0} wheels",
+            newWheels);
+        wheels = newWheels;
+    }
+    
+    public void Display()
+    {
+        Console.WriteLine("Hi, im a car and i have {0} wheels", 
+            wheels);
+    }
+    
+    ~Car()
+    {
+        Console.WriteLine("Destroying a car");
+    }
+}
+
+class Ex257
+{
+    public static void Main()
+    {
+        Car alfa = new Car();
+        Car lancia = new Car(6);
+
+        alfa.Display();  
+        lancia.Display();  
+    }
+}
