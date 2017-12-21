0a1f8a8d764a85f32ce23c178ae6ce49108ca54b
Nacho Cabanes <ncabanes@users.noreply.github.com>
2017-12-21 16:09:41 +0100

    Ex.261: Class SocialNetwork

---
+++ chapter06-classes/261-SocialNetwork.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+class SocialNetwork
+{
+    protected string name;
+    protected string url;
+    protected int foundationYear;
+
+    public SocialNetwork(string newName, 
+        string newUrl, int newFoundationYear)
+    {
+        name = newName;
+        url = newUrl;
+        foundationYear = newFoundationYear;
+    }
+
+    public void SetName(string newName)
+    {
+        name = newName;
+    }
+
+    public string GetName()
+    {
+        return name;
+    }
+
+    public void SetUrl(string newUrl)
+    {
+        name = newUrl;
+    }
+
+    public string GetUrl()
+    {
+        return url;
+    }
+
+    public void SetFoundationYear (int newFoundationYear)
+    {
+        foundationYear = newFoundationYear;
+    }
+
+    public int GetFoundationYear()
+    {
+        return foundationYear;
+    }
+}
+
+// --------------------------------
+
+class MyTest
+{
+    public static void Main()
+    {
+        SocialNetwork fb = new SocialNetwork(
+            "Facebook", "facebook.com", 2004);
+        Console.WriteLine(fb.GetName() + " was founded on "
+            + fb.GetFoundationYear() );
+    }
+}
