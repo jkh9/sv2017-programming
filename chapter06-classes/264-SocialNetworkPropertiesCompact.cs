2efcad03438a7031e6297edf7687cc464dd8bd60
Nacho Cabanes <ncabanes@users.noreply.github.com>
2017-12-21 17:00:39 +0100

    Ex.264: Class SocialNetwork+Properties (compact)

---
+++ chapter06-classes/264-SocialNetworkPropertiesCompact.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class SocialNetwork
+{
+    public SocialNetwork(string newName, 
+        string newUrl, int newFoundationYear)
+    {
+        Name = newName;
+        Url = newUrl;
+        FoundationYear = newFoundationYear;
+    }
+
+    public string Name { get; set; }
+    public string Url { get; set; }
+    public int FoundationYear { get; set; }
+}
+
+// --------------------------------
+
+class MyTest
+{
+    public static void Main()
+    {
+        SocialNetwork fb = new SocialNetwork(
+            "Facebook", "facebook.com", 2004);
+        Console.WriteLine(fb.Name + " was founded on "
+            + fb.FoundationYear );
+    }
+}
