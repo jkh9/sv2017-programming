7a49fd2b4cb558f206928465b047203a75737bb1
Nacho Cabanes <ncabanes@users.noreply.github.com>
2017-12-21 17:01:09 +0100

    Ex.265: Class SocialNetwork+Properties + byte

---
+++ chapter06-classes/265-SocialNetworkPropertiesMixed.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class SocialNetwork
+{
+    protected byte foundationYear;
+    
+    public SocialNetwork(string newName, 
+        string newUrl, int newFoundationYear)
+    {
+        Name = newName;
+        Url = newUrl;
+        FoundationYear = newFoundationYear;
+    }
+
+    public string Name { get; set; }
+    public string Url { get; set; }
+    
+    public int FoundationYear
+    {
+        get { return foundationYear + 1980; }
+        set { 
+            foundationYear =             
+                Convert.ToByte(value - 1980);
+            }
+    }
+}
+
+// --------------------------------
+
+class MyTest
+{
+    public static void Main()
+    {
+        SocialNetwork fb = new SocialNetwork(
+            "Facebook", "facebook.com", 2004);
+        Console.WriteLine(fb.Name + " was founded on "
+            + fb.FoundationYear );
+    }
+}
