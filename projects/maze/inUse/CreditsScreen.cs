f7239dcf9b5bbaa24d183cf1617ef39d56cb2b70
Nacho Cabanes <nacho@nachocabanes.com>
2018-01-07 19:29:45 +0100

    Version 008 of the Maze project

--- projects/maze/inUse/CreditsScreen.cs
+++ projects/maze/inUse/CreditsScreen.cs
@@ -3,6 +3,7 @@
  *  Maze Game
  * 
  *  V0.07 19-Dic-2017 Nacho: Almost empty skeleton
+ *  V0.08 20-Dic-2017 Lucia, HP, Daniel: Some text...
  */
  
  using System;
@@ -12,8 +13,48 @@ public class CreditsScreen
 
     public void Display()
     {
-        Console.Write("Credits soon available. Press Enter to continue...");
+        int i = 2;
+        Console.Clear();
+        Console.SetCursorPosition(20, 0);
+        Console.BackgroundColor = ConsoleColor.White;
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine("MAZE");
+        Console.ResetColor();
+        Console.BackgroundColor = ConsoleColor.Red;
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.SetCursorPosition(14, i);
+        Console.WriteLine("·Creator: Fifteen");
+        i++;
+        Console.SetCursorPosition(14, i);
+        Console.WriteLine("·Designer: Sahrien");
+        i++;
+        Console.SetCursorPosition(14, i);
+        Console.WriteLine("·Programmer: HP96");
+        i++;
+        Console.SetCursorPosition(14, i);
+        Console.WriteLine("·Music: There is no music yet");
+        i++;
+        Console.SetCursorPosition(14, i);
+        Console.WriteLine("·No human being has suffered");
+        i++;
+        Console.SetCursorPosition(14, i);
+        Console.WriteLine("     physical damage with the");
+        i++;
+        Console.SetCursorPosition(14, i);
+        Console.WriteLine("     creation of this project.");
+        i++;
+        Console.SetCursorPosition(14, i);
+        Console.WriteLine("·All the human beings who have");
+        i++;
+        Console.SetCursorPosition(14, i);
+        Console.WriteLine("     participated in this project have ");
+        i++;
+        Console.SetCursorPosition(14, i);
+        Console.WriteLine("     suffered psychological damage");
+        
+        Console.ResetColor();
         Console.ReadLine();
+        Console.Clear();
     }
 
 }
--- projects/maze/inUse/RoomViewer.cs
+++ projects/maze/inUse/RoomViewer.cs
@@ -2,17 +2,68 @@
  *  Maze Game
  * 
  *  V0.07 19-Dic-2017 Nacho: Empty skeleton
+ *  V0.08 20-Dic-2017 Javier Saorín, Raúl Gogna, Miguel Pastor: Display rooms.
  */
 
 using System;
 
 public class RoomViewer
 {
+    enum orientations { NORTH, EAST, SOUTH, WEST };
 
-    public void Display()
+    public void Display(int actualOrientation, string[,] map, int x, int y)
     {
-        throw new System.Exception("Not implemented yet!");
+        if (actualOrientation == (byte)orientations.SOUTH &&
+            map[y, x].Contains("D"))
+            DisplayDoor();
+        else if (actualOrientation == (byte)orientations.WEST &&
+            map[y, x].Contains("L"))
+            DisplayDoor();
+        else if (actualOrientation == (byte)orientations.NORTH &&
+            map[y, x].Contains("U"))
+            DisplayDoor();
+        else if (actualOrientation == (byte)orientations.EAST &&
+            map[y, x].Contains("R"))
+            DisplayDoor();
+        else
+            DisplayWall();
+    }
+    
+    public void DisplayDoor()
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine();
+        Console.WriteLine(" \\                   /");
+        Console.WriteLine("  \\                 /");
+        Console.WriteLine("   -----------------");
+        Console.WriteLine("  |                 |");
+        Console.WriteLine("  |                 |");
+        Console.WriteLine("  |        ___      |");
+        Console.WriteLine("  |       |   |     |");
+        Console.WriteLine("  |       |   |     |");
+        Console.WriteLine("   -----------------");
+        Console.WriteLine("  /                 \\");
+        Console.WriteLine(" /                   \\");
+        Console.WriteLine();
+        Console.ResetColor();
     }
 
+    public void DisplayWall()
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine();
+        Console.WriteLine(" \\                   /");
+        Console.WriteLine("  \\                 /");
+        Console.WriteLine("   -----------------");
+        Console.WriteLine("  |                 |");
+        Console.WriteLine("  |                 |");
+        Console.WriteLine("  |                 |");
+        Console.WriteLine("  |                 |");
+        Console.WriteLine("  |                 |");
+        Console.WriteLine("   -----------------");
+        Console.WriteLine("  /                 \\");
+        Console.WriteLine(" /                   \\");
+        Console.WriteLine();
+        Console.ResetColor();
+    }
 }
-
