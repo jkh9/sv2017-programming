5f8aa21ff2912a49c87a382913f6cf3956a1ce36
Nacho Cabanes <ncabanes@users.noreply.github.com>
2018-01-08 19:11:50 +0100

    Ex.267: Contact with "static" (2)

---
+++ chapter07-advancedOOP/267-MotorbikeStatic2.cs
@@ -0,0 +1,42 @@
+﻿/*
+Create a class named “Motorbike”.
+It will have an attribute “wheels”, static, with value 2.
+Create a method named “GetWheels”, to access that attribute.
+Create two motorbikes and display how many wheels they have.
+*/
+
+using System;
+
+class Motorbike
+{
+    protected static int wheels = 2;
+    
+    public int GetWheels()
+    {
+        return wheels;
+    }
+    
+    public void SetWheels(int w)
+    {
+        wheels = w;
+    }
+}
+
+// --------------------------------
+
+class MotorbikeTest
+{
+    static void Main()
+    {
+        Motorbike m1 = new Motorbike();
+        Motorbike m2 = new Motorbike();
+        
+        Console.WriteLine("m1 has {0} wheels", m1.GetWheels() );
+        Console.WriteLine("m2 has {0} wheels", m2.GetWheels() );
+        
+        m1.SetWheels(3);
+        
+        Console.WriteLine("m1 has {0} wheels", m1.GetWheels() );
+        Console.WriteLine("m2 has {0} wheels", m2.GetWheels() );
+    }
+}
