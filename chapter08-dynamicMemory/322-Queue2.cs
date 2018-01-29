b65988e182ff38f7da05abdf517b8cd8fcef9daf
Nacho Cabanes <ncabanes@users.noreply.github.com>
2018-01-29 19:51:38 +0100

    Ex.322: Contact with Queues (2)

---
+++ chapter08-dynamicMemory/322-Queue2.cs
@@ -0,0 +1,42 @@
+﻿/*
+Ask the user for as many strings as they like, using a Queue to store the data 
+temporarily. When the user enters an empty string, you must display all the 
+strings entered so far and start asking for more strings. The program will 
+finish when the user enters “quit”
+*/
+
+// Moisés Encinas
+
+using System;
+using System.Collections;
+
+class Program
+{
+    static void Main()
+    {
+        Queue myQueue = new Queue();
+        string option;
+        do
+        {
+            do
+            {
+                option = Console.ReadLine();
+                if (option != "" && option != "quit")
+                {
+                    myQueue.Enqueue(option);
+                }
+            } 
+            while (option != "" && option != "quit");
+
+            if (option != "quit")
+            {
+                int amount = myQueue.Count;
+                for (int i = 0; i < amount; i++)
+                {
+                    Console.WriteLine(myQueue.Dequeue());
+                }
+            }
+
+        } while (option != "quit");
+    }
+}
