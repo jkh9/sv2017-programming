aa80fd04c475b13f5f6b633b2a657f40db6f5c4d
Nacho Cabanes <ncabanes@users.noreply.github.com>
2018-02-09 18:40:30 +0100

    Ex.354: Translator, Hoygan to Spanish

---
+++ chapter08-dynamicMemory/354a-Hoygan1.cs
@@ -0,0 +1,35 @@
+﻿/*
+Create a C# program to translate from Hoygan to Spanish, 
+using a SortedList. It must work correctly with the following sentence:
+
+hoygan porfabor nesecito alluda grasias
+*/
+
+using System;
+using System.Collections;
+
+public class Hoygan
+{
+    public static void Main()
+    {
+        SortedList myDictionary = new SortedList();
+        myDictionary.Add("hoygan", "oigan");
+        myDictionary.Add("porfabor", "por favor");
+        myDictionary.Add("nesecito", "necesito");
+        myDictionary.Add("alluda", "ayuda");
+        myDictionary.Add("grasias", "gracias");
+
+        Console.Write("Enter a sentence: ");
+        string text = Console.ReadLine();
+
+        string[] parts = text.ToLower().Split();
+
+        foreach (string part in parts)
+        {
+            if (myDictionary.ContainsKey(part))
+                Console.Write(myDictionary[part] + " ");
+            else
+                Console.Write(part + " ");
+        }
+    }
+}
---
+++ chapter08-dynamicMemory/354c-Hoygan3.cs
@@ -0,0 +1,34 @@
+﻿/*
+Create a C# program to translate from Hoygan to Spanish, 
+using a SortedList. It must work correctly with the following sentence:
+
+hoygan porfabor nesecito alluda grasias
+*/
+
+using System;
+using System.Collections;
+
+public class Hoygan
+{
+    public static void Main()
+    {
+        Hashtable myDictionary = new Hashtable();
+        myDictionary.Add("hoygan", "oigan");
+        myDictionary.Add("porfabor", "por favor");
+        myDictionary.Add("nesecito", "necesito");
+        myDictionary.Add("alluda", "ayuda");
+        myDictionary.Add("grasias", "gracias");
+
+        Console.Write("Enter a sentence: ");
+        string text = Console.ReadLine();
+
+        string[] parts = text.ToLower().Split();
+        for (int i = 0; i < parts.Length; i++)
+        {
+            if (myDictionary.ContainsKey(parts[i]))
+                parts[i] = (string) myDictionary[ parts[i] ];
+        }
+
+        Console.Write( String.Join(" ", parts)) ;
+    }
+}
