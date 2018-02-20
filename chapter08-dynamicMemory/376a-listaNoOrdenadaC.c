6154f11da552e23433088b4960e330674a06a7b9
Nacho Cabanes <nacho@nachocabanes.com>
2018-02-20 10:36:45 +0100

    Ex.376: List, C-style

---
+++ chapter08-dynamicMemory/376a-listaNoOrdenadaC.c
@@ -0,0 +1,59 @@
+﻿/* Lista enlazada (no ordenada) en C */
+ 
+#include <stdio.h>
+#include <stdlib.h>
+ 
+struct lista               /* Nuestra lista */
+{
+    int numero;             /* Solo guarda un numero */
+    struct lista* sig;      /* Y el puntero al siguiente dato */
+};
+ 
+struct lista* CrearLista(int valor)  /* Crea la lista, claro */
+{
+    struct lista* r;        /* Variable auxiliar */
+    r = (struct lista*) 
+        malloc (sizeof(struct lista)); /* Reserva memoria */                           
+    r->numero = valor;      /* Guarda el valor */
+    r->sig = NULL;          /* No hay siguiente */
+    return r;               /* Crea el struct lista* */
+}
+ 
+void MostrarLista ( struct lista *lista ) 
+{
+    if (lista) {                          /* Si realmente hay lista */
+        printf("%d\n", lista->numero);    /* Escribe el valor */
+        MostrarLista (lista->sig );       /* Y mira el siguiente */
+    }
+}
+ 
+void InsertarLista( struct lista **lista, int valor) 
+{
+    struct lista* r;        /* Variable auxiliar, para reservar */
+    struct lista* actual;   /* Otra auxiliar, para recorrer */
+    
+    r = CrearLista(valor);  /* Creamos un nuevo nodo */
+    actual = *lista;        /* Y partimos desde el princio */
+    
+    if (!actual)                           
+    {                       /* Si no hay lista, hay que crearla */
+        *lista = r;         /* y hay que indicar donde debe comenzar */
+    }
+    else                    /*  Si hay lista */
+    {
+        while (actual->sig)
+            actual = actual->sig;  /* Avanzo hasta el final */
+        actual->sig = r;           /* Y el nuevo nodo será el final */
+    }
+}
+ 
+int main() 
+{
+    struct lista* l;               /* La lista que crearemos */
+    l = CrearLista(5);             /* Crea una lista e introduce un 5 */
+    InsertarLista(&l, 3);          /* Inserta un 3 */
+    InsertarLista(&l, 2);          /* Inserta un 2 */
+    InsertarLista(&l, 6);          /* Inserta un 6 */
+    MostrarLista(l);               /* Muestra la lista resultante */
+    return 0;                      /* Se acabó */
+}
---
+++ chapter08-dynamicMemory/376b-listaNoOrdenadaCsharp.cs
@@ -0,0 +1,83 @@
+﻿// Lista enlazada (no ordenada) en Csharp
+
+using System;
+
+class Lista
+{
+
+    public class Nodo            // Cada nodo de la lista
+    {
+          public int dato;       // - Un dato (int)
+          public Nodo siguiente; // - Y el enlace al siguiente dato
+    }
+
+    Nodo raiz = null;
+
+    public Lista(int valor)
+    {
+        raiz = CrearNodo(valor);
+    }
+    
+    private Nodo CrearNodo(int valor)
+    {
+        Nodo n = new Nodo();     // Reservo memoria
+        n.dato = valor;          // Guardo el valor
+        n.siguiente = null;      // Y no hay siguiente
+        return n;
+    }
+
+    public void Escribir( )   // Escribir, desde la raíz
+    {
+        Escribir (raiz);
+    }
+
+    private void Escribir( Nodo n )  // Escribe desde cierto nodo (recursivo)
+    {
+        if (n != null) 
+        {                               // Si hay datos desde aquí
+            Console.Write(n.dato+" ");  //  Escribo el dato del nodo
+            Escribir(n.siguiente);      //  Y luego miro el siguiente
+        }
+    }
+
+    public void Insertar( int valor) 
+    {
+        Insertar(ref raiz, valor);
+    }
+        
+    private void Insertar( ref Nodo nodoInicial, int valor) 
+    {
+        Nodo nuevoNodo;  // Variable auxiliar, para reservar
+        Nodo actual;     // Otra auxiliar, para recorrer
+        
+        nuevoNodo = CrearNodo(valor);
+        actual = nodoInicial;
+        
+        if (actual == null)
+        {                             // Si no hay lista, hay que crearla
+            nodoInicial = nuevoNodo;  // y hay que indicar donde debe comenzar
+        }
+        else                          // Si ya hay datos,
+        {
+            while (actual.siguiente != null)
+                actual = actual.siguiente;  // Avanzo hasta el final
+            actual.siguiente = nuevoNodo;   // Y el nuevo nodo será el final
+        }
+    }
+}
+
+
+public class PruebaLista
+{
+    //  Cuerpo del programa
+    public static void Main()
+    {
+        Lista l;           // La lista que crearemos
+        l = new Lista(5);  // Crea una lista e introduce un 5
+        l.Insertar(3);
+        l.Insertar(2);
+        l.Insertar(6);
+        l.Insertar(1);
+        l.Escribir();       // Muestra la lista resultante
+    }
+}
