6f7bfedd2a59ed059c97d87c8ac77c94752094f3
Nacho Cabanes <nacho@nachocabanes.com>
2018-02-21 23:27:34 +0100

    Ex.380: Simple BASIC to C# converter, two versions

---
+++ chapter09-files/380a-BASICToCsharp1.cs
@@ -0,0 +1,91 @@
+﻿// Create a program to read a simple BASIC source file like 
+// the following one and convert it to C#:
+// 
+// 10 PRINT "Enter a number"
+// 20 INPUT n1
+// 30 PRINT "Enter another number"
+// 40 INPUT secondNumber
+// 50 PRINT "Their sum is"
+// 60 PRINT n1+secondNumber
+
+//Almudena Lopez Sanchez
+//Feb 21st c (Ex.380)- Simple BASIC to C#
+
+using System;
+using System.IO;
+
+public class BASICToCsharp
+{
+    public unsafe static void Main()
+    {
+        Console.WriteLine("Enter a BASIC file: ");
+        string file = Console.ReadLine();
+
+        if (!File.Exists(file))
+            Console.WriteLine("File not exists...");
+        else
+        {
+            try
+            {
+                StreamReader sr = new StreamReader(file);
+                string newFile = file;
+                if (!file.Contains("."))
+                    newFile += ".cs";
+                else
+                {
+                    newFile = newFile.Substring(0,
+                        newFile.LastIndexOf('.')) + ".cs";
+                }
+
+                StreamWriter sw = new StreamWriter(newFile);
+                string line;
+                sw.WriteLine("//almu.chan");
+                sw.WriteLine("using System;");
+                sw.WriteLine("public class Program");
+                sw.WriteLine("{");
+                sw.WriteLine("    public static void Main()");
+                sw.WriteLine("    {");
+                do
+                {
+                    line = sr.ReadLine();
+                    
+                    if (line != null)
+                    {
+                        line = line.Remove(0, line.IndexOf(" ") + 1);
+
+                        if (line.StartsWith("PRINT"))
+                        {
+                            line = "        Console.WriteLine(" +
+                                line.Substring(6) + 
+                                ");";
+                        }
+
+                        else if (line.StartsWith("INPUT"))
+                        {
+                            line = "        int " +
+                                line.Substring(6) + 
+                                " = Convert.ToInt32(Console.ReadLine());";
+                        }
+                        sw.WriteLine(line);
+                    }
+                }
+                while (line != null);
+
+                sr.Close();
+                sw.WriteLine("    }");
+                sw.WriteLine("}");
+                sw.Close();
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine("I/O error: " + e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("ERROR: " + e.Message);
+            }
+        }
+        
+    }
+}
+
---
+++ chapter09-files/380b-BASICToCsharp2.cs
@@ -0,0 +1,73 @@
+﻿// Create a program to read a simple BASIC source file like 
+// the following one and convert it to C#:
+// 
+// 10 PRINT "Enter a number"
+// 20 INPUT n1
+// 30 PRINT "Enter another number"
+// 40 INPUT secondNumber
+// 50 PRINT "Their sum is"
+// 60 PRINT n1+secondNumber
+
+//Raul Gogna
+
+using System;
+using System.IO;
+
+class Ex380
+{
+    public static void Main()
+    {
+        Console.Write("Enter file name: ");
+        string inputFile = Console.ReadLine();
+        if(!File.Exists(inputFile))
+            Console.WriteLine("File does not exist");
+        else
+        {
+            try
+            {
+                StreamReader reader = new StreamReader(inputFile);
+
+                StreamWriter writer = File.CreateText(inputFile + ".cs");
+                writer.WriteLine("//Raul Gogna");
+                writer.WriteLine("using System;");
+                writer.WriteLine("class Ex380");
+                writer.WriteLine("{");
+                writer.WriteLine("    public static void Main()");
+                writer.WriteLine("    {");
+                string line;
+                do
+                {
+                    line = reader.ReadLine();
+                    if(line != null)
+                    {
+                        line = line.Remove(0, line.IndexOf(" ") + 1);
+                        if (line.Contains("PRINT"))
+                        {
+                            writer.Write("        ");
+                            writer.WriteLine("Console.WriteLine(" + 
+                                line.Substring(6) + ");");
+                        }
+                        if(line.Contains("INPUT"))
+                        {
+                            writer.Write("        ");
+                            writer.WriteLine("int " + line.Substring(6) + 
+                                " = Convert.ToInt32(Console.ReadLine());");
+                        }
+                    }
+                } while (line != null);
+                writer.WriteLine("    }");
+                writer.WriteLine("}");
+                writer.Close();
+                reader.Close();
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("I/O error: " + e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unexpected error: " + e.Message);
+            }
+        }
+    }
+}
