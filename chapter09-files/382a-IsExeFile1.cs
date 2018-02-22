9191bb34ca853340f646b445c47d502beaeb7123
Nacho Cabanes <ncabanes@users.noreply.github.com>
2018-02-22 16:01:59 +0100

    Ex.382: Is EXE file? (two versions)

---
+++ chapter09-files/382a-IsExeFile1.cs
@@ -0,0 +1,65 @@
+//Pedro Luis Coloma
+
+using System;
+using System.IO;
+
+public class ExeOrNotExe
+{
+    public static void Main()
+    {
+        Console.Write("Enter a file name: ");
+        string name = Console.ReadLine();
+
+        if (!File.Exists(name))
+        {
+            Console.WriteLine("File not found!");
+        }
+        else
+        {
+            try
+            {
+                FileStream myFile = File.OpenRead(name);
+                byte data;
+                bool firstLetterCorrect = false;
+                bool secondLetterCorrect = false;
+
+                data = (byte) myFile.ReadByte();                
+                if (data == 'M')
+                {
+                    firstLetterCorrect = true;
+                }
+                
+                data = (byte) myFile.ReadByte();
+                if (data == 'Z')
+                {
+                    secondLetterCorrect = true;
+                }
+
+                if (firstLetterCorrect && secondLetterCorrect)
+                {
+                    Console.WriteLine("Seems a real EXE file.");
+                }
+                else
+                {
+                    Console.WriteLine("It's not an EXE file");
+                }
+            }
+            catch (PathTooLongException)
+            {
+                Console.WriteLine("Path too long");
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("File not accessible");
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("I/O error: " + e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Oooops... " + e.Message);
+            }
+        }
+    }
+}
