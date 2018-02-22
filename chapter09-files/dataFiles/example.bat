54d859f84682a57176798a9e8966d6cbcc52c2b4
Nacho Cabanes <nacho@nachocabanes.com>
2018-02-22 11:26:01 +0100

    Several example data files

---
+++ chapter09-files/dataFiles/example.bat
@@ -0,0 +1,6 @@
+CLS
+echo Mirando lista de directorio superior
+Cd ..
+dir > lista.txt
+NOTEPAD lista.txt
+Echo Ordenes permitidas: CLS, ECHO, CD
---
+++ chapter09-files/dataFiles/example.sql
@@ -0,0 +1,3 @@
+insert into personas (nombre, direccion, edad) values ('smith, pedro', 'su calle', 23);
+insert into personas (nombre, direccion, edad) values ('juan', 'calle cinco, 6', 24);
+insert into ciudades (codigo, nombre) values ('a', 'alicante');
