3bd066a564c02fca5e004912de8691831deacb68
Nacho Cabanes <nacho@nachocabanes.com>
2018-03-02 00:50:56 +0100

    PointOfSale 0.11

--- projects/pos/inUse/SalesModule.cs
+++ projects/pos/inUse/SalesModule.cs
@@ -12,21 +12,26 @@
 // V0.07 29-Nov-2017 Nacho: Simple file usage
 // V0.08 19-Dic-2017 Nacho: Split into classes, most content moved to SalesModule
 // V0.09 17-Ene-2018 Nacho: Date for each transaction is also saved
+// V0.11 28-Feb-2018 Guillermo Pastor, Pedro Luis Coloma, Renata Pestana, Javier Cases: 
+//         Arrays replaced for Lists<struct>
 
 using System;
 using System.IO;
+using System.Collections.Generic;
+
+public struct Sells
+{
+    public double dailyTransaction;
+    public DateTime date;
+}
 
 public class SalesModule
 {
     public void Run()
     {
-        double[] dailyTransactions = new double[1000];
-        DateTime[] dates = new DateTime[1000];
+        List<Sells> transaction = new List<Sells>();
 
-        int amountOfTransactions = 0;
-
-        LoadFromFile(ref dailyTransactions,ref dates,
-            ref amountOfTransactions);
+        transaction = LoadFromFile(transaction);
 
         Console.WriteLine("Hint: Enter the amount sold, "
             + "press Enter to get the total, or "
@@ -45,11 +50,11 @@ public class SalesModule
 
                 try
                 {
+                    Sells s;
                     amount = Convert.ToDouble(answer);
                     sum += amount;
-                    dailyTransactions[amountOfTransactions] = amount;
-                    dates[amountOfTransactions] = DateTime.Now;
-                    amountOfTransactions++;
+                    s.dailyTransaction = amount;
+                    s.date = DateTime.Now;
                 }
                 catch (Exception)
                 {
@@ -61,19 +66,19 @@ public class SalesModule
             Console.WriteLine("Total: " + sum);
             sum = 0;
 
-            if ((answer == "total") || (answer == "total"))
+            if (answer == "total")
             {
                 double total = 0;
-                for (int i = 0; i < amountOfTransactions; i++)
+                for (int i = 0; i < transaction.Count; i++)
                 {
-                    total += dailyTransactions[i];
+                    total += transaction[i].dailyTransaction;
                 }
                 Console.WriteLine("Daily total: " + total);
             }
         }
         while (answer != "end");
 
-        SaveToFile(dailyTransactions, dates, amountOfTransactions);
+        SaveToFile(transaction);
     }
 
 
@@ -84,32 +89,31 @@ public class SalesModule
     }
 
 
-    public static void LoadFromFile(ref double[] dailyTransactions,
-        ref DateTime[] dates,
-        ref int amountOfTransactions)
+    public static List<Sells> LoadFromFile(List<Sells> temp)
     {
         if (File.Exists("pos.dat"))
         {
             string[] dataFromFile = File.ReadAllLines("pos.dat");
             for (int i = 0; i < dataFromFile.Length; i++)
             {
-                dailyTransactions[i] = Convert.ToDouble(dataFromFile[i].Split('@')[1]);
-                dates[i] = Convert.ToDateTime(dataFromFile[i].Split('@')[0]);
+                Sells s;
+                s.dailyTransaction = Convert.ToDouble(dataFromFile[i].Split('@')[1]);
+                s.date = Convert.ToDateTime(dataFromFile[i].Split('@')[0]);
+                temp.Add(s);
             }
-            amountOfTransactions = dataFromFile.Length;
+            return temp;
         }
+        return temp;
     }
 
 
-    public static void SaveToFile(
-        double[] dailyTransactions, DateTime[] dates,
-        int amountOfTransactions)
+    public static void SaveToFile(List<Sells> temp)
     {
-        string[] dataToFile = new string[amountOfTransactions];
-        for (int i = 0; i < amountOfTransactions; i++)
+        string[] dataToFile = new string[temp.Count];
+        for (int i = 0; i < temp.Count; i++)
         {
-            dataToFile[i] = dates[i] + "@" +
-                Convert.ToString(dailyTransactions[i]);
+            dataToFile[i] = temp[i].date + "@" +
+                Convert.ToString(temp[i].dailyTransaction);
         }
         File.WriteAllLines("pos.dat", dataToFile);
     }
