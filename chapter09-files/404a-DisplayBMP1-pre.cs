75763ca6f5422f74203d03cb90702ab28d970601
Nacho Cabanes <nacho@nachocabanes.com>
2018-03-06 11:01:26 +0100

    Ex.404: Display a simple BMP in console (2 versions)

---
+++ chapter09-files/404a-DisplayBMP1-pre.cs
@@ -0,0 +1,66 @@
+﻿//Moisés Encinas Picazo
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+public class Ex404
+{
+    public static void Main(string[] args)
+    {
+        string fileName;
+        Console.WriteLine("Name of the file");
+        fileName = "welcome8.bmp";
+
+        if (!File.Exists(fileName))
+        {
+            Console.WriteLine("File not found");
+        }
+        else
+        {
+            try
+            {
+                BinaryReader input = new BinaryReader(new FileStream(fileName, FileMode.Open));
+                byte b = input.ReadByte();
+                byte m = input.ReadByte();
+
+                if (b == 'B' && m == 'M')
+                {
+                    input.BaseStream.Seek(10,SeekOrigin.Begin);
+                    int start = input.ReadInt32();
+
+                    input.BaseStream.Seek(18, SeekOrigin.Begin);
+                    int width = input.ReadInt32();
+
+                    input.BaseStream.Seek(start, SeekOrigin.Begin);
+                    for (int i = start; i < input.BaseStream.Length; i++)
+                    {
+                        byte line = input.ReadByte();
+                        if ((i+1) % width == width -1)
+                        {
+                            Console.WriteLine();
+                        }
+                        if (line > 127)
+                        {
+                            Console.Write(" ");
+                        }
+                        else
+                        {
+                            Console.Write("*");
+                        }
+                    }
+                }
+                input.Close();
+            }
+            catch (IOException)
+            {
+
+            }
+
+            catch (Exception e)
+            {
+
+            }
+        }
+    }
+}
---
+++ chapter09-files/404b-DisplayBMP2.cs
@@ -0,0 +1,75 @@
+﻿//Moisés Encinas Picazo
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+public class Ex404
+{
+    public static void Main(string[] args)
+    {
+        string fileName;
+        Console.WriteLine("Name of the file");
+        fileName = "welcome8.bmp";
+
+        if (!File.Exists(fileName))
+        {
+            Console.WriteLine("File not found");
+        }
+        else
+        {
+            try
+            {
+                BinaryReader input = new BinaryReader(new FileStream(fileName, FileMode.Open));
+                byte b = input.ReadByte();
+                byte m = input.ReadByte();
+
+                if (b == 'B' && m == 'M')
+                {
+                    input.BaseStream.Seek(10,SeekOrigin.Begin);
+                    int start = input.ReadInt32();
+
+                    input.BaseStream.Seek(18, SeekOrigin.Begin);
+                    int width = input.ReadInt32();
+
+                    input.BaseStream.Seek(start, SeekOrigin.Begin);
+                    List<string> data = new List<string>();
+                    string line = "";
+                    for (int i = start; i < input.BaseStream.Length; i++)
+                    {
+                        byte bite = input.ReadByte();
+                        if ((i+1) % width == width -1)
+                        {
+                            data.Add(line);
+                            line = "";
+                            Console.WriteLine();
+                        }
+                        if (bite > 127)
+                        {
+                            line += " ";
+                        }
+                        else
+                        {
+                            line += "*";
+                        }
+                    }
+
+                    for (int i = data.Count - 1; i >= 0; i--)
+                    {
+                        Console.WriteLine(data[i]);
+                    }
+                }
+                input.Close();
+            }
+            catch (IOException)
+            {
+
+            }
+
+            catch (Exception e)
+            {
+
+            }
+        }
+    }
+}
