7561b77fb470dda3dee8fd2a09e5be95d904e1fd
Nacho Cabanes <nacho@nachocabanes.com>
2018-03-13 10:32:03 +0100

    Ex.412: Contact with persistence

---
+++ chapter10-persistence/412-PersistenceSerialization.cs
@@ -0,0 +1,83 @@
+﻿/*
+Create a new version of the class Item, using serialization
+
+(Use a different file name: formats will not be the same)
+*/
+
+using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+[Serializable]
+public class Item
+{
+	protected string description;
+	protected double price;
+	
+	public Item(string description, double price)
+	{
+		this.description = description;
+		this.price = price;
+	}
+	
+	public void SetPrice(double p)
+	{
+		price = p;
+	}
+	
+	public double GetPrice()
+	{
+		return price;
+	}
+	
+	public void SetDescription(string d)
+	{
+		description = d;
+	}
+	
+	public string GetDescription()
+	{
+		return description;
+	}
+	
+	public static void Save(Item i) {
+		IFormatter formatter = new BinaryFormatter();
+		Stream stream = new FileStream("item2.dat",
+			FileMode.Create, FileAccess.Write, 
+			FileShare.None);
+		formatter.Serialize(stream, i);
+		stream.Close();
+	}
+	
+	public static Item Load() 
+	{
+		Item i;
+		IFormatter formatter = new BinaryFormatter();
+		Stream stream = new FileStream("item2.dat", 
+			FileMode.Open, FileAccess.Read, 
+			FileShare.Read);
+		i = (Item) formatter.Deserialize(stream);
+		stream.Close();
+		return i;
+	}
+	
+}	
+
+public class ItemTest
+{	
+    public static void Main()
+    {
+        Item i = new Item("test", 10);
+        Console.WriteLine(i.GetDescription() + " " +
+			i.GetPrice());
+		Item.Save(i);
+		
+		i = new Item("test 2", 20);
+        Console.WriteLine(i.GetDescription() + " " +
+			i.GetPrice());
+		i = Item.Load();
+		Console.WriteLine(i.GetDescription() + " " +
+			i.GetPrice());
+    }
+}
