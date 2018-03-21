a5ecb831fbf5120435d5529b695103e69d95d429
Nacho Cabanes <ncabanes@users.noreply.github.com>
2018-03-21 18:00:35 +0100

    Ex.422: Contact with SQLite 2, SELECT

---
+++ chapter11-databases/422a-SQLite02.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data.SQLite;
+
+public class SQLite02
+{
+    public static void Main(string[] args)
+    {
+        SQLiteConnection connection =
+            new SQLiteConnection
+            ("Data Source=cars01.sqlite;Version=3;New=False; Compress=True;");
+        connection.Open();
+        string query = "select * from cars";
+        SQLiteCommand cmd = new SQLiteCommand(query, connection);
+        SQLiteDataReader data = cmd.ExecuteReader();
+        while (data.Read())
+        {
+            string brand = Convert.ToString(data[0]);
+            string model = Convert.ToString(data[1]);
+            int power = Convert.ToInt32(data[2]);
+            Console.WriteLine("Brand: " + brand);
+            Console.WriteLine("Model: "+model);
+            Console.WriteLine("Power: " + power);
+        }
+        connection.Close();
+    }
+}
