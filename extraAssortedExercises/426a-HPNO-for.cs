05145d80f249303c56b59bdfef3ca526a4d5687e
Nacho Cabanes <nacho@nachocabanes.com>
2018-03-22 23:28:07 +0100

    Exercise 426: Variations of four letters (2 versions)

---
+++ extraAssortedExercises/426a-HPNO-for.cs
@@ -0,0 +1,31 @@
+﻿// Display all the variations with the letters H, P, N, O, 
+// maybe repeated (and count them)
+
+// Javier Cases
+
+using System;
+
+public class Tema11
+{
+    public static void Main()
+    {
+        char[] letters = new char[4];
+        letters[0] = 'h';
+        letters[1] = 'p';
+        letters[2] = 'n';
+        letters[3] = 'o';
+
+        int count = 0;
+
+        for (int i = 0; i < letters.Length; i++)
+            for (int j = 0; j < letters.Length; j++)
+                for (int w = 0; w < letters.Length; w++)
+                    for (int x = 0; x < letters.Length; x++)
+                    {
+                        Console.WriteLine("" + letters[i] + 
+                            letters[j] + letters[w] + letters[x]);
+                        count++;
+                    }
+        Console.WriteLine(count);
+    }
+}
---
+++ extraAssortedExercises/426b-HPNO-recursion.cs
@@ -0,0 +1,28 @@
+﻿// Display all the variations with the letters H, P, N, O, 
+// maybe repeated (and count them)
+
+using System;
+
+public class Tema11
+{
+    public static void DisplayValues(string start, 
+        string alphabet, int validLength)
+    {
+        // End: if length > 4
+        if (start.Length > validLength)
+            return;
+        
+        // Solution:  if length == 4
+        if (start.Length == validLength)
+            Console.Write(start+ " ");
+            
+        // Otherwise, keep searching
+        foreach(char c in alphabet)
+            DisplayValues(start+c, alphabet, validLength);
+    }
+    
+    public static void Main()
+    {
+        DisplayValues("", "hpno", 4);
+    }
+}
