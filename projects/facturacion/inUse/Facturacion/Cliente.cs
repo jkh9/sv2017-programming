7913ef429b8ecd3902782e7913da0e72c7a8dd88
Nacho Cabanes <nacho@nachocabanes.com>
2018-04-14 14:25:11 +0200

    Facturación, versión 0.02 - Cliente y ListaDeClientes

---
+++ projects/facturacion/inUse/Facturacion/Cliente.cs
@@ -0,0 +1,35 @@
+﻿// Facturación, clase "Cliente"
+
+// Versiones:
+// V0.02a 11-Abr-2018 Raúl Gogna: Propiedades y constructor
+
+class Cliente
+{
+    public string Nombre { get; set; }
+    public string Cif { get; set; }
+    public string Domicilio { get; set; }
+    public string Ciudad { get; set; }
+    public string CodigoPostal { get; set; }
+    public string Pais { get; set; }
+    public string Telefono { get; set; }
+    public string Email { get; set; }
+    public string Contacto { get; set; }
+    public string Observaciones { get; set; }
+
+    public Cliente(string nombre, string cif, string domicilio, string ciudad,
+        string codigoPostal, string pais, string telefono, string email,
+        string contacto, string observaciones)
+    {
+        Nombre = nombre;
+        Cif = cif;
+        Domicilio = domicilio;
+        Ciudad = ciudad;
+        CodigoPostal = codigoPostal;
+        Pais = pais;
+        Telefono = telefono;
+        Email = email;
+        Contacto = contacto;
+        Observaciones = observaciones;
+    }
+
+}
---
+++ projects/facturacion/inUse/Facturacion/ListaDeClientes.cs
@@ -0,0 +1,35 @@
+﻿// Facturación, clase "ListaDeClientes"
+
+// Versiones:
+// V0.02a 11-Abr-2018 Raúl Gogna, correcciones por Nacho: Constructor, Add, Get, Count
+
+using System.Collections.Generic;
+
+class ListaDeClientes
+{
+    private List<Cliente> clientes;
+    public int Count { get { return clientes.Count; }  }
+
+    public ListaDeClientes()
+    {
+        clientes = new List<Cliente>();
+    }
+
+    public void Add(Cliente cliente)
+    {
+        clientes.Add(cliente);
+    }
+
+    public Cliente Get(int n)
+    {
+        if (n >= clientes.Count || n < 0)
+        {
+            return null;
+        }
+        else
+        {
+            return clientes[n];
+        }
+    }
+}
+
