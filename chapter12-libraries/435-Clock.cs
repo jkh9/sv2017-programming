c80ac97b06e4c9b16065bb4e3dd8ae4000189974
Nacho Cabanes <nacho@nachocabanes.com>
2018-04-17 15:14:53 +0200

    Ex.435: Clock in the upper right corner

---
+++ chapter12-libraries/435-Clock.cs
@@ -0,0 +1,24 @@
+﻿// Javier Saorín Vidal
+
+using System;
+
+public class DateTest
+{
+    public static void Main()
+    {
+        bool exit = false;
+        do
+        {
+            DateTime date = DateTime.Now;
+
+            Console.SetCursorPosition(71, 0);
+            Console.WriteLine(
+                date.Hour.ToString("00") + ":" +
+                date.Minute.ToString("00") + ":" + 
+                date.Second.ToString("00"));
+
+            System.Threading.Thread.Sleep(1000);
+        }
+        while (!exit);
+    }
+}
