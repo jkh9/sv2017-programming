c0b3087354b237fe6b2ac8fbf9cd4b8ca41086ed
Nacho Cabanes <ncabanes@users.noreply.github.com>
2018-04-18 17:45:39 +0200

    Ex.440: Tasks every 7 days

---
+++ chapter12-libraries/440a-Tasks7days1.cs
@@ -0,0 +1,32 @@
+﻿// Javier Cases Sempere
+
+using System;
+using System.IO;
+
+class Tasks7days
+{
+    static void Main(string[] args)
+    {
+         string[] month = 
+         { 
+             "Jan", "Feb", "Mar", "Apr", 
+             "May", "Jun", "Jul", "Aug",
+             "Sep", "Oct", "Nov", "Dec" 
+        };
+        
+        StreamWriter writer = new StreamWriter("tasks.txt");
+        DateTime d = DateTime.Now.AddDays(7);
+        int currentYear = d.Year;
+        do
+        {
+            writer.WriteLine(
+                d.Day.ToString("00") + "-" + 
+                month[d.Month - 1] + "-" + 
+                d.Year % 100);
+            d = d.AddDays(7);
+        }
+        while (d.Year == currentYear);
+        writer.Close();
+    }
+}
+
---
+++ chapter12-libraries/440b-Tasks7days2.cs
@@ -0,0 +1,23 @@
+﻿// Javier Cases Sempere
+
+using System;
+using System.IO;
+
+class Tasks7days
+{
+    static void Main(string[] args)
+    {
+        StreamWriter writer = new StreamWriter("tasks.txt");
+        DateTime d = DateTime.Now.AddDays(7);
+        int currentYear = d.Year;
+        do
+        {
+            string month = d.ToString("MMM").Substring(0, 1).ToUpper() 
+                + d.ToString("MMM").Substring(1, 2);
+            writer.WriteLine(d.Day + " - " + month + " - " + d.Year);
+            d = d.AddDays(7);
+        }
+        while (d.Year == currentYear);
+        writer.Close();
+    }
+}
