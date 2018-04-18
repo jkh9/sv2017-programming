a5d1aa9bd074867161e04e9b341e8fedf300ef64
Nacho Cabanes <ncabanes@users.noreply.github.com>
2018-04-18 17:06:49 +0200

    Ex.439: Today + 7 days

---
+++ chapter12-libraries/439-Today7Days.cs
@@ -0,0 +1,12 @@
+//Marcos Cervantes Matamoros
+
+using System;
+
+public class Days7
+{
+    public static void Main()
+    {
+        DateTime date = DateTime.Now.AddDays(7);
+        Console.WriteLine(date);
+    }
+}
