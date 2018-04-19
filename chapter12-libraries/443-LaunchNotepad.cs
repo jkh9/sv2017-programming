2ec54e9c013a159e633853a490be49dcbcf84e02
Nacho Cabanes <nacho@nachocabanes.com>
2018-04-19 11:25:11 +0200

    Ex.443: Running an external program

---
+++ chapter12-libraries/443-LaunchNotepad.cs
@@ -0,0 +1,15 @@
+﻿//  Cesar Martin Sogorb
+
+using System;
+using System.Diagnostics;
+
+public class RunProgram
+{
+    public static void Main()
+    {
+        Console.Write("Enter a filename: ");
+        string fileName = Console.ReadLine();
+        Process proc = Process.Start("notepad.exe", fileName);
+        proc.WaitForExit();
+    }
+}
