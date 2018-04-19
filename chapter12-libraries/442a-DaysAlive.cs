7369025811cd9e059bb41129397bf32d43ee10df
Nacho Cabanes <nacho@nachocabanes.com>
2018-04-19 11:13:45 +0200

    Ex.442: Days alive (and seconds alive)

---
+++ chapter12-libraries/442a-DaysAlive.cs
@@ -0,0 +1,26 @@
+﻿// Miguel García Gil
+// Days alive
+
+using System;
+using System.IO;
+
+public class DaysAlive
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Day of birth?");
+        int day = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Month of birth?");
+        int month = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Year of birth?");
+        int year = Convert.ToInt32(Console.ReadLine());
+
+        DateTime today = DateTime.Today;
+        DateTime birthDate = new DateTime(year, month, day);
+
+        TimeSpan diff = today.Subtract(birthDate);
+        Console.WriteLine("{0} days elapsed",diff.Days);
+    }
+}
---
+++ chapter12-libraries/442b-SecondsAlive.cs
@@ -0,0 +1,26 @@
+﻿// Miguel García Gil
+// Seconds alive
+
+using System;
+using System.IO;
+
+public class SecondsAlive
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Day of birth?");
+        int day = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Month of birth?");
+        int month = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Year of birth?");
+        int year = Convert.ToInt32(Console.ReadLine());
+
+        DateTime today = DateTime.Today;
+        DateTime birthDate = new DateTime(year, month, day);
+
+        TimeSpan diff = today.Subtract(birthDate);
+        Console.WriteLine("Approx {0} seconds elapsed",diff.TotalSeconds);
+    }
+}
