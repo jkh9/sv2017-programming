52ad2f76268a27fc357dd0477c8f6cc34798a63e
Nacho Cabanes <ncabanes@users.noreply.github.com>
2018-04-26 19:51:53 +0200

    Ex.455: Contact with LINQ

---
+++ chapter12-libraries/455a-LINQ1a.cs
@@ -0,0 +1,32 @@
+﻿// Create a program to ask the user for 10 numbers, store them in an array 
+// and display the positive ones, sorted ascending, using LINQ
+
+// Javier Cases Sempere
+
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class Linq1
+{
+    static void Main(string[] args)
+    {
+        List<double> nums = new List<double>();
+
+        for (int i = 0; i < 10; i++)
+        {
+            Console.Write("Enter a number ("+(i+1)+
+                "/10): ");
+            nums.Add(Convert.ToDouble(Console.ReadLine()));
+        }
+
+        var result =
+            from n in nums
+            where n > 0
+            orderby n
+            select n;
+
+        foreach (double i in result)
+            Console.Write(i + " ");
+    }
+}
---
+++ chapter12-libraries/455b-LINQ1b.cs
@@ -0,0 +1,32 @@
+﻿// Create a program to ask the user for 10 numbers, store them in an array 
+// and display the positive ones, sorted ascending, using LINQ
+
+//Moiséx Encinas Picazo
+
+using System;
+using System.Diagnostics;
+using System.Linq;
+
+public class Linq1
+{
+    public static void Main(string[] args)
+    {
+        double[] numbers = new double[10];
+        for (int i = 0; i < 10; i++)
+        {
+            Console.Write("Enter a number ("+(i+1)+
+                "/10): ");
+            numbers[i] = Convert.ToDouble(Console.ReadLine());
+        }
+
+        var results = from n in numbers
+                      where n > 0
+                      orderby n
+                      select n;
+
+        foreach (var item in results)
+        {
+            Console.Write(item + " ");
+        }
+    }
+}
