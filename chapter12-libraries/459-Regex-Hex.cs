d44b8f2a23719d271ea86266161e3b175a500b41
Nacho Cabanes <ncabanes@users.noreply.github.com>
2018-04-27 20:56:05 +0200

    Ex.459: IsHex, regular expressions

---
+++ chapter12-libraries/459-Regex-Hex.cs
@@ -0,0 +1,23 @@
+﻿// Javier Cases Sempere
+
+using System;
+using System.Text.RegularExpressions;
+
+class RegExHex
+{
+    public static bool IsHex(string s)
+    {
+        Regex r = new Regex(@"\A[0-9a-fA-F]+\z");
+        return r.IsMatch(s);
+    }
+
+    public static void Main(string[] args)
+    {
+        string text = Console.ReadLine();
+
+        if (IsHex(text))
+            Console.WriteLine("Valid hex");
+        else
+            Console.WriteLine("Not valid hex");
+    }
+}
