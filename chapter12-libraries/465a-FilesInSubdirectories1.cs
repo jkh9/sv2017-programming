a920e416cf04b9c6f498ef14887869ca95978bcb
Nacho Cabanes <nacho@nachocabanes.com>
2018-04-30 23:07:01 +0200

    Ex.465: Files in subdirectories1

---
+++ chapter12-libraries/465a-FilesInSubdirectories1.cs
@@ -0,0 +1,38 @@
+﻿/*
+Create a program to display the names of the files contained in the 
+current directory and its subdirectories which follow a certain 
+wildcard (for example, "*.cs"). 
+*/
+
+using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string filter = args.Length < 1 ? "*.*" : args[0];
+        string currentDir = Directory.GetCurrentDirectory();
+        ShowContent(currentDir, filter);
+    }
+
+    public static void ShowContent( string dir, string filter)
+    {
+        Console.WriteLine(dir);
+        String[] files = Directory.GetFiles(dir, filter);
+        foreach (string f in files)
+        {
+            Console.WriteLine("  "+f);
+        }
+
+        String[] paths = Directory.GetDirectories(dir, filter);
+        foreach( string f in paths)
+        {
+            ShowContent(f, filter);
+        }
+        
+        
+
+    }
+}
+
