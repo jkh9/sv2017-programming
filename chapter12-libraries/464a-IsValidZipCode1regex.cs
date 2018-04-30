3dbd1cf4660b5a6948c9d60025ab8a4128c5905d
Nacho Cabanes <nacho@nachocabanes.com>
2018-04-30 23:02:29 +0200

    Ex. 464: IsValidZipCode, RegEx

---
+++ chapter12-libraries/464a-IsValidZipCode1regex.cs
@@ -0,0 +1,23 @@
+﻿// Javier Cases Sempere
+
+using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    public static bool IsValidZipCode(string s)
+    {
+        Regex r = new Regex(@"\A[0-5][0-9]{4}\z");
+        return r.IsMatch(s);
+    }
+
+    public static void Main(string[] args)
+    {
+        string text = Console.ReadLine();
+
+        if (IsValidZipCode(text))
+            Console.WriteLine("Valid");
+        else
+            Console.WriteLine("Not valid");
+    }
+}
