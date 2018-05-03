b98f8e54d37bf82e0240c347b1eadc7af044aeea
Nacho Cabanes <ncabanes@users.noreply.github.com>
2018-05-03 18:50:39 +0200

    Ex.474: LINQ 3 (method syntax)

---
+++ chapter12-libraries/474a-LINQ3a.cs
@@ -0,0 +1,37 @@
+﻿// Create a program to ask the user for 10 numbers, store them 
+// in an array and display the positive ones, sorted ascending, 
+// using LINQ
+
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class Linq1
+{
+    static void Main(string[] args)
+    {
+        List<double> nums = new List<double>();
+
+        for (int i = 0; i < 10; i++)
+        {
+            Console.Write("Enter a number ("+(i+1)+
+                "/10): ");
+            nums.Add(Convert.ToDouble(Console.ReadLine()));
+        }
+
+        /*
+        var result =
+            from n in nums
+            where n > 0
+            orderby n
+            select n;
+		*/
+
+        var result = nums
+			.Where(n => n > 0)
+			.OrderBy(n => n);
+        
+        foreach (double i in result)
+            Console.Write(i + " ");
+    }
+}
