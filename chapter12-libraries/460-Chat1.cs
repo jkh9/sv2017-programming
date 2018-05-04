6851bedac4b9e09f7ca444e7612dbaae84c8b24b
Nacho Cabanes <nacho@nachocabanes.com>
2018-05-04 11:55:04 +0200

    Ex.460 : Simple network communications

---
+++ chapter12-libraries/460-Chat1.cs
@@ -0,0 +1,61 @@
+﻿// Javier Cases Sempere
+
+using System;
+using System.IO; // Para Stream
+using System.Text; // Para Encoding
+using System.Net; // Para Dns, IPAddress
+using System.Net.Sockets; // Para NetworkStream
+
+public class ComunicacionRed
+{
+    static string direccionPrueba = "localhost";
+    static int puertoPrueba = 2112;
+    private static void enviar(string direccion,
+    int puerto, string frase)
+    {
+        TcpClient cliente = new TcpClient(direccion, puerto);
+        NetworkStream conexion = cliente.GetStream();
+
+        byte[] secuenciaLetras = Encoding.ASCII.GetBytes(frase);
+        conexion.Write(secuenciaLetras, 0, secuenciaLetras.Length);
+        conexion.Close();
+        cliente.Close();
+    }
+    private static string esperar(string direccion, int puerto)
+    {
+        // Tratamos de hallar la primera IP que corresponde
+        // a una dirección como "localhost"
+
+        IPAddress direccionIP = Dns.GetHostEntry(direccion).AddressList[0];
+    
+            // Comienza la espera de información
+        TcpListener listener = new TcpListener(direccionIP, puerto);
+        listener.Start();
+        TcpClient cliente = listener.AcceptTcpClient();
+        NetworkStream conexion = cliente.GetStream();
+        StreamReader lector = new StreamReader(conexion);
+
+        string frase = lector.ReadToEnd();
+
+        cliente.Close();
+        listener.Stop();
+        return frase;
+    }
+    public static void Main()
+    {
+        Console.WriteLine("Pulse 1 para recibir o 2 para enviar");
+        string respuesta = Console.ReadLine();
+        if (respuesta == "2") // Enviar
+        {
+            Console.Write("Enviando... ");
+            enviar(direccionPrueba, puertoPrueba, "Prueba de texto");
+            Console.WriteLine("Enviado");
+        }
+        else // Recibir
+        {
+            Console.WriteLine("Esperando... ");
+            Console.WriteLine(esperar(direccionPrueba, puertoPrueba));
+            Console.WriteLine("Recibido");
+        }
+    }
+}
